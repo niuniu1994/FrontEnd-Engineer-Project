4fb61834bb3efc3a75e3bb504f59ce3e9f01f75c
Kobe19 <fguirol@gmail.com>
2021-12-16 18:08:00 +0100

    Debut dev page de connexion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './view/pages/login-page/login-page.component';
+// import { HomePageComponent } from './view/pages/home-page/home-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    // HomePageComponent
   ],
   imports: [
     BrowserModule,
